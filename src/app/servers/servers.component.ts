0769115319530970fa69060dd86a5b9dfc695f0d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-03 14:55:35 -0600

    added comments diffrent ways of importing select

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+    /* other ways of writing the selecor
+    * [app-server] // and the way you write it in the template is <div app-server ></div>
+    * .app-server // and the way you write it in the template is <div class="app-server"></div>
+    and the way to use i */ 
   template: `
   <app-server></app-server>
   <app-server></app-server>`,
