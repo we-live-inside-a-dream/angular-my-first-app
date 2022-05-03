7bbb35cd48e7342350ce05add60bc71ec6787059
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-03 14:54:25 -0600

    added new components to app component and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AssignmentComponent } from './assignment/assignment.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    AssignmentComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
