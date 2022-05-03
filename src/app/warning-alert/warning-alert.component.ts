b2947694f97a9dd6dd6395f307292f076dafe4c8
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-03 14:55:55 -0600

    created warning alert component and styled it

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert.component.html',
+    styles: [
+        `
+        p {
+            color: 20px;
+            background-color: mistyrose;
+            border: 1px solid red;
+
+        }
+        `
+    ]
+})
+export class WarningAlertComponent{
+
+}
