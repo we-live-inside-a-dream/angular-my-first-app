64c1624425a4de3c1052068cb3ae65d61c5afcbe
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-04 15:18:08 -0600

    created event binding functions

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,16 +6,30 @@ import { Component, OnInit } from '@angular/core';
     * [app-server] // and the way you write it in the template is <div app-server ></div>
     * .app-server // and the way you write it in the template is <div class="app-server"></div>
     and the way to use i */ 
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = ''
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+   
 
   ngOnInit(): void {
   }
-
+  onCreateServer(): void {
+    this.serverCreationStatus = 'Server was created!';
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value
+  }
 }
