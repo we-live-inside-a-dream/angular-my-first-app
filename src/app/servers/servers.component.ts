5568e0325196f7b3290bfd15ce3641852d74a99f
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-04 15:49:02 -0600

    chnaged sneding data with event bining to two-way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,7 +15,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = ''
+  serverName = 'Test Server';
 
   constructor() {
     setTimeout(() => {
@@ -27,11 +27,11 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
   onCreateServer(): void {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! ' + this.serverName;
   }
   onUpdateServerName(event: any) {
     this.serverName = event.target.value
     console.log(event)
-    
+
   }
 }
