f571a7431e5419d9b78f3e89788941c9035c53ff
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-06 13:46:29 -0600

    created a toggle button loging all the clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showDetails = false
+  logClick = [];
+  
+  
+
+
+  constructor() {
+    
+  }
+
+  onDisplayDetails() {
+    this.showDetails = !this.showDetails
+    this.logClick.push(this.logClick.length + 1)
+  }
+  
+
 }
